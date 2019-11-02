6011c1947cd9f1c05e5713b02a872e4362358ff0
Salvatore Ventrone <ventrosky@gmail.com>
2019-11-02 11:48:36 +0100

    firesale chpt 6
    supports drag-and-drop functionality, watches the filesystem for changes, adds files to the operating system’s list of recently opened files, updates the window’s title bar, sets a represented file on macOS, and alerts the user before discarding unsaved changes;
    
    extra:
    * Patched BrowserWindow prototypes .setDocumentEdited .isDocumentEdited for cross cross-platform compatbility
    * Made use of Promises for dialogs
    * Work around empty DataTransfer fields
    * fs.watch reports events twice, replaced with chokidar

--- firesale/app/main.js
+++ firesale/app/main.js
@@ -1,26 +1,95 @@
 const { app, BrowserWindow, dialog } = require('electron');
 const fs = require('fs');
+const chokidar = require('chokidar');
 const path = require('path');
 
+const openFiles = new Map();
 const windows = new Set();
 
-const openFile = (targetWindow, file) => {
+const openFile = exports.openFile = (targetWindow, file) => {
     const content = fs.readFileSync(file).toString();
+    startWatchingFile(targetWindow, file);
+    app.addRecentDocument(file);
+    targetWindow.setRepresentedFilename(file);
     targetWindow.webContents.send('file-opened', file, content);
 };
 
-const getFileFromUser =  exports.getFileFromUser = async (targetWindow) => {
-    const files = await dialog.showOpenDialog(targetWindow, {
+const getFileFromUser =  exports.getFileFromUser = (targetWindow) => {
+    dialog.showOpenDialog(targetWindow, {
         properties: ['openFile'],
         filters: [
             { name: 'Text Files', extensions: ['txt'] },
             { name: 'Markdown Files', extensions: ['md', 'markdown'] }
         ]
+    }).then(files => {
+        if (files.filePaths.length != 0) { 
+            openFile(targetWindow, files.filePaths[0]); 
+        }
+    }).catch(function(error) {
+        console.error(error);
     });
-    if (files.filePaths.length != 0) { 
-        openFile(targetWindow, files.filePaths[0]); 
+};
+
+const saveHtml = exports.saveHtml = (targetWindow, content) => {
+    dialog.showSaveDialog(targetWindow, {
+        title: 'Save HTML',
+        defaultPath: app.getPath('documents'),
+        filters: [
+            { name: 'HTML Files', extensions: ['html', 'htm'] }
+        ]
+    }).then(file => {
+        if (file.filePath == '') return;
+        fs.writeFileSync(file.filePath, content);
+    }).catch(function(error) {
+        console.error(error);
+    });
+};
+
+const saveMarkdown = exports.saveMarkdown = (targetWindow, file, content) => {
+    if (! file) {
+        dialog.showSaveDialog(targetWindow, {
+            title: 'Save Markdown',
+            defaultPath: app.getPath('documents'),
+            filters: [
+                { name: 'Markdown Files', extensions: ['md', 'markdown'] }
+            ]
+        }).then(fileObj => {
+            if (fileObj.filePath == '') return;
+            fs.writeFileSync(fileObj.filePath, content);
+        }).catch(function(error) {
+            console.error(error);
+        });
+    } else {
+        fs.writeFileSync(file, content);
+        openFile(targetWindow, file);
+    }
+}
+
+const startWatchingFile = (targetWindow, file) => {
+    stopWatchingFile(targetWindow);
+    const watcher = chokidar.watch(file).on('change', (path, stats) => {
+        const content = fs.readFileSync(file).toString();
+        targetWindow.webContents.send('file-changed', file, content);
+    });
+    openFiles.set(targetWindow, watcher);
+};
+    
+const stopWatchingFile = (targetWindow) => {
+    if (openFiles.has(targetWindow)) {
+        openFiles.get(targetWindow).close().then(() => openFiles.delete(targetWindow));;
     }
 };
+
+// patching methods only available under macOS
+if (process.platform !== 'darwin') {
+    BrowserWindow.prototype.setDocumentEdited = function(isEdited) {
+        this.firesaleEdit = isEdited;
+    };
+    BrowserWindow.prototype.isDocumentEdited = function(){ 
+        return this.firesaleEdit == undefined ? false : this.firesaleEdit
+    };
+}
+
 
 const createWindow = exports.createWindow = () => {
     let x, y;
@@ -39,16 +108,42 @@ const createWindow = exports.createWindow = () => {
             nodeIntegration: true
         }
     });
+
     newWindow.loadFile(path.resolve('app/index.html'));
     //`file://${__dirname}/index.html`
     newWindow.once('ready-to-show', () => {
+        newWindow.firesaleEdit = false;
         newWindow.show();
         newWindow.webContents.openDevTools();
     });
+    
+    newWindow.on('close', (event) => {
+        if (newWindow.isDocumentEdited()) {
+            event.preventDefault();
+            dialog.showMessageBox(newWindow, {
+                type: 'warning',
+                title: 'Quit with Unsaved Changes?',
+                message: 'Your changes will be lost if you do not save.',
+                buttons: [
+                    'Quit Anyway',
+                    'Cancel',
+                ],
+                defaultId: 0,
+                cancelId: 1
+            }).then(result => {
+                if (result.response === 0) newWindow.destroy()
+            }).catch(function(error) {
+                console.error(error);
+            });
+        }
+    });
+
     newWindow.on('closed', () => {
         windows.delete(newWindow);
+        stopWatchingFile(newWindow);
         newWindow = null;
     });
+    
     windows.add(newWindow);
     return newWindow;
 };
@@ -67,3 +162,12 @@ app.on('window-all-closed', () => {
 app.on('activate', (event, hasVisibleWindows) => {
     if (!hasVisibleWindows) { createWindow(); }
 });
+
+app.on('will-finish-launching', () => {
+    app.on('open-file', (event, file) => {
+        const win = createWindow();
+        win.once('ready-to-show', () => {
+            openFile(win, file);
+        });
+    });
+})
--- firesale/app/renderer.js
+++ firesale/app/renderer.js
@@ -1,5 +1,8 @@
 const marked = require('marked');
+const sanitizeHtml = require('sanitize-html');
+
 const { remote, ipcRenderer  } = require('electron');
+const path = require('path');
 const mainProcess = remote.require('./main.js');
 
 const currentWindow = remote.getCurrentWindow();
@@ -14,13 +17,80 @@ const saveHtmlButton = document.querySelector('#save-html');
 const showFileButton = document.querySelector('#show-file');
 const openInDefaultButton = document.querySelector('#open-in-default');
 
+let filePath = null;
+let originalContent = '';
+
+document.addEventListener('dragstart', event => event.preventDefault());
+document.addEventListener('dragover', event => event.preventDefault());
+document.addEventListener('dragleave', event => event.preventDefault());
+document.addEventListener('drop', event => event.preventDefault());
+
+const getDraggedFile = (event) => event.dataTransfer.items[0];
+
+const getDroppedFile = (event) => event.dataTransfer.files[0];
+
+const fileTypeIsSupported = (file) => file.type != '' ? ['text/plain', 'text/markdown'].includes(file.type) : /\.(md|markdown|txt)$/i.test(file.name);
+
 const renderMarkdownToHtml = (markdown) => {
-    htmlView.innerHTML = marked(markdown, { sanitize: true });
+    var clean = sanitizeHtml(markdown);
+    htmlView.innerHTML = marked(clean);
+};
+
+const updateUserInterface = (isEdited) => {
+    let title = 'Fire Sale';
+
+    if (filePath) { 
+        title = `${path.basename(filePath)} - ${title}`; 
+    };
+    if (isEdited) { 
+        title = `${title} (Edited)`; 
+    };
+
+    currentWindow.setTitle(title);
+    currentWindow.setDocumentEdited(isEdited);
+    saveMarkdownButton.disabled = !isEdited;
+    revertButton.disabled = !isEdited;
+};
+
+const renderFile = (file, content) => {
+    console.log(content)
+    filePath = file;
+    originalContent = content;
+    markdownView.value = content;
+    currentWindow.setDocumentEdited(false);
+    renderMarkdownToHtml(content);
+    updateUserInterface(false);
 };
 
 markdownView.addEventListener('keyup', (event) => {
     const currentContent = event.target.value;
     renderMarkdownToHtml(currentContent);
+    updateUserInterface(currentContent !== originalContent);
+});
+
+markdownView.addEventListener('dragover', (event) => {
+    const file = getDraggedFile(event);
+    if (fileTypeIsSupported(file)) {
+        markdownView.classList.add('drag-over');
+    } else {
+        markdownView.classList.add('drag-error');
+    }
+});
+
+markdownView.addEventListener('dragleave', () => {
+    markdownView.classList.remove('drag-over');
+    markdownView.classList.remove('drag-error');
+});
+
+markdownView.addEventListener('drop', (event) => {
+    const file = getDroppedFile(event);
+    if (fileTypeIsSupported(file)) {
+        mainProcess.openFile(currentWindow, file.path);
+    } else {
+        alert('That file type is not supported');
+    }
+    markdownView.classList.remove('drag-over');
+    markdownView.classList.remove('drag-error');
 });
 
 openFileButton.addEventListener('click', () => {
@@ -28,10 +98,60 @@ openFileButton.addEventListener('click', () => {
 });
 
 ipcRenderer.on('file-opened', (event, file, content) => {
-    markdownView.value = content;
-    renderMarkdownToHtml(content);
+    if (currentWindow.isDocumentEdited()) {
+        remote.dialog.showMessageBox(currentWindow, {
+            type: 'warning',
+            title: 'Overwrite Current Unsaved Changes?',
+            message: 'Opening a new file in this window will overwrite your unsaved changes. Open this file anyway?',
+            buttons: [
+                'Yes',
+                'Cancel',
+            ],
+            defaultId: 0,
+            cancelId: 1
+        }).then(result => {
+            if (result.response === 1) renderFile(file, content);
+        }).catch(function(error) {
+            console.error(error);
+        });
+    } else {
+        renderFile(file, content)
+    }
+});
+
+ipcRenderer.on('file-changed', (event, file, content) => {
+    console.log("received changes")
+    remote.dialog.showMessageBox(currentWindow, {
+        type: 'warning',
+        title: 'Overwrite Current Unsaved Changes?',
+        message: 'Another application has changed this file. Load changes?',
+        buttons: [
+            'Yes',
+            'Cancel',
+        ],
+        defaultId: 0,
+        cancelId: 1
+    }).then(result => {
+        if (result.response === 0) renderFile(file, content);
+    }).catch(function(error) {
+        console.error(error);
+    });
 });
 
 newFileButton.addEventListener('click', () => {
     mainProcess.createWindow();
+});
+
+saveHtmlButton.addEventListener('click', () => {
+    mainProcess.saveHtml(currentWindow, `<!DOCTYPE html><html><head></head><body>${htmlView.innerHTML}</body></html>`);
+});
+
+saveMarkdownButton.addEventListener('click', () => {
+    mainProcess.saveMarkdown(currentWindow, filePath, markdownView.value);
+});
+
+revertButton.addEventListener('click', () => {
+    currentWindow.setDocumentEdited(false);
+    markdownView.value = originalContent;
+    renderMarkdownToHtml(originalContent);
 });
